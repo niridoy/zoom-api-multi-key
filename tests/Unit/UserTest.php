f00caead8fdfca46f98e1e27dd872a24c272f673
spondonit2 <spn2@spondonit.com>
2021-05-24 17:31:01 +0600

    Intial Commit

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MacsiDigital\Zoom\Tests\Unit;
+
+use MacsiDigital\Zoom\Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /** @test */
+    public function testCase()
+    {
+        $this->assertTrue(true);
+    }
+}
